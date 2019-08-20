4a34da19dfa3fe37f188b652a8b773ba7ff134d6
Axel Martin <axel.steph.martin@gmail.com>
2019-08-20 17:04:44 -0400

    Make bin update
    1) added dplyr to our imports
    2) new options in create.bin.mat to let users keep only somatic muts, remove silent ones or keep only snps (see manual for details)

--- R/make-bin-mat.R
+++ R/make-bin-mat.R
@@ -5,11 +5,19 @@
 #'
 #' @param patients a character vector that let's the user specify the patients to be used to create the matrix.
 #' Default is NULL is which case all patients in the MAF file will be used.
-#' @param Fits All fits from the OncoCast run.
+#' @param maf A MAF file.
+#' @param mut.type The mutation type to be used. Options are "SOMATIC", "GERMLINE" or "ALL". Note "ALL" will
+#' keep all mutations regardless of status (not recommended). Default is SOMATIC.
+#' @param SNP.only Boolean to rather the genetics events to be kept only to be SNPs (insertions and deletions will be removed).
+#' Default is FALSE.
+#' @param include.silent Boolean to keep or remove all silent mutations. TRUE keeps, FALSE removes. Default is FALSE.
 #' @return mut : a binary matrix of mutation data
+#' @return no.mu.patients : a character vector of patients having no mutations found in the MAF file.
 #' @export
+#'
+#' @import dplyr
 
-create.bin.matrix <- function(patients=NULL, maf){
+create.bin.matrix <- function(patients=NULL, maf, mut.type = "SOMATIC",SNP.only = F,include.silent = F){
 
   # quick data checks #
   if(length(match("Tumor_Sample_Barcode",colnames(maf))) == 0)
@@ -18,10 +26,22 @@ create.bin.matrix <- function(patients=NULL, maf){
     stop("The MAF file inputted is missing a gene name column. (Hugo_Symbol)")
   if(length(match("Variant_Classification",colnames(maf))) == 0)
     stop("The MAF file inputted is missing a variant classification column. (Variant_Classification)")
+  if(length(match("Mutation_Status",colnames(maf))) == 0)
+    stop("The MAF file inputted is missing a mutation status column. (Mutation_Status)")
 
-
+  # filter/define patients #
   if(!is.null(patients)) maf <- maf[maf$Tumor_Sample_Barcode %in% patients,]
-  maf <- maf[maf$Variant_Classification != "Silent",]
+  else patients <- as.character(unique(maf$Tumor_Sample_Barcode))
+
+  # clean gen dat #
+  if(SNP.only) SNP.filt = "SNP" else SNP.filt = unique(maf$Variant_Type)
+  if(!include.silent) Variant.filt = "Silent" else Variant.filt = ""
+  if(mut.type == "ALL") Mut.filt = unique(maf$Mutation_Status) else Mut.filt = mut.type
+
+  maf <- maf %>% filter(Variant_Classification != Variant.filt,
+                        Variant_Type %in% SNP.filt,
+                        Mutation_Status %in% Mut.filt)
+
   #### out frame
   mut <- as.data.frame(matrix(0L,nrow=length(patients),ncol=length(unique(maf$Hugo_Symbol))))
   colnames(mut) <- unique(maf$Hugo_Symbol)
