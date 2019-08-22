8ee05f16e07f07fc3bc9ee0fd8581805fe59f03b
Axel Martin <axel.steph.martin@gmail.com>
2019-08-22 15:03:48 -0400

    Added function for oncoprint
    dat.oncoPrint is a helper function to create the correct format for a oncoprint plot using as input the output of the create.bin.matrix so things are streamlined
    plot.oncoprint calls dat.oncoPrint to actually plot the oncoprint (which it returns)

--- R/make-oncoprint-dat.R
+++ R/make-oncoprint-dat.R
@@ -11,18 +11,19 @@
 
 dat.oncoPrint <- function(gen.dat,clin.dat=NULL){
 
-  # subset data #
-  patients <- intersect(rownames(gen.dat),rownames(clin.dat))
-  gen.dat <- gen.dat[match(patients,rownames(gen.dat)),]
-  clin.dat <- clin.dat[match(patients,rownames(clin.dat)),]
-
-  ## names ##
-  genes <- c(colnames(clin.dat),unique(gsub(".Amp|.Del|.fus","",colnames(gen.dat))))
+  if(!is.null(clin.dat)){
+    # subset data #
+    patients <- intersect(rownames(gen.dat),rownames(clin.dat))
+    gen.dat <- gen.dat[match(patients,rownames(gen.dat)),]
+    clin.dat <- clin.dat[match(patients,rownames(clin.dat)),]
+    ## names ##
+    genes <- c(colnames(clin.dat),unique(gsub(".Amp|.Del|.fus","",colnames(gen.dat))))
+  }
+  else genes <- unique(gsub(".Amp|.Del|.fus","",colnames(gen.dat)))
 
   mat <- as.data.frame(matrix(nrow=length(genes),ncol=nrow(gen.dat)))
   colnames(mat) <- rownames(gen.dat)
   rownames(mat) <- genes
-  # mat[is.na(mat)] <- "  "
 
   # mutations #
   mut <- gen.dat[,na.omit(match(genes,colnames(gen.dat)))]
@@ -31,23 +32,19 @@ dat.oncoPrint <- function(gen.dat,clin.dat=NULL){
     for(j in 1:ncol(mut)){
       if(!is.na(mut[i,j])){
         if(mut[i,j]==1) {
-          if(rownames(mat)[match(colnames(mut)[j],rownames(mat))] == "WGD") mat[match(colnames(mut)[j],rownames(mat)),match(rownames(mut)[i],colnames(mat))] <- "WGD;"
-          else{mat[match(colnames(mut)[j],rownames(mat)),match(rownames(mut)[i],colnames(mat))] <- "MUT;"}
+          mat[match(colnames(mut)[j],rownames(mat)),match(rownames(mut)[i],colnames(mat))] <- "MUT;"
         }
-
-        if(rownames(mat)[match(colnames(mut)[j],rownames(mat))] == "TMB"){
-          if(mut[i,j] < as.numeric(quantile(gen.dat$TMB,0.33)) ){mat[match(colnames(mut)[j],rownames(mat)),match(rownames(mut)[i],colnames(mat))] <- "TMB_low;"}
-          else if(mut[i,j] > as.numeric(quantile(gen.dat$TMB,0.66)) ){mat[match(colnames(mut)[j],rownames(mat)),match(rownames(mut)[i],colnames(mat))] <- "TMB_high;"}
-          else{mat[match(colnames(mut)[j],rownames(mat)),match(rownames(mut)[i],colnames(mat))] <- "TMB_int;"}
-        }
-
       }
     }
   }
 
   # del #
   del <- gen.dat[,na.omit(match(paste0(genes,".Del"),colnames(gen.dat)))]
-  colnames(del) <- gsub(".Del","",colnames(del))
+  if(is.null(dim(del))) {
+    del <- as.data.frame(del)
+    rownames(del) <- rownames(gen.dat)
+  }
+  colnames(del) <- gsub(".Del","",colnames(gen.dat)[na.omit(match(paste0(genes,".Del"),colnames(gen.dat)))])
   for(i in 1:nrow(del)){
     for(j in 1:ncol(del)){
       if(del[i,j]==1) {
@@ -61,7 +58,11 @@ dat.oncoPrint <- function(gen.dat,clin.dat=NULL){
 
   # amp #
   amp <- gen.dat[,na.omit(match(paste0(genes,".Amp"),colnames(gen.dat)))]
-  colnames(amp) <- gsub(".Amp","",colnames(amp))
+  if(is.null(dim(amp))) {
+    amp <- as.data.frame(amp)
+    rownames(amp) <- rownames(gen.dat)
+  }
+  colnames(amp) <- gsub(".Amp","",colnames(gen.dat)[na.omit(match(paste0(genes,".Amp"),colnames(gen.dat)))])
   for(i in 1:nrow(amp)){
     for(j in 1:ncol(amp)){
       if(amp[i,j]==1) {
@@ -73,27 +74,44 @@ dat.oncoPrint <- function(gen.dat,clin.dat=NULL){
     }
   }
 
-
-  ### clin ###
-
-  temp <- clin.dat
-  for(x in colnames(clin.dat)){
-    y <- clin.dat[,x]
-
-    # if binary #
-    if(length(unique(y[!is.na(y)])) == 2){
-      mat[match(x,rownames(mat)),] <- ifelse(y==1,"CLIN;",NA)
-    }
-
-    else if(length(unique(y[!is.na(y)])) %in% c(3:5)){
-      mat[match(x,rownames(mat)),] <- paste0(abbreviate(x,1),y,";")
-    }
-
-    else{
-      mat[match(x,rownames(mat)),] <- ifelse(y > median(y),"CLIN;",NA)
+  # fusions #
+  fusion <- gen.dat[,na.omit(match(paste0(genes,".fus"),colnames(gen.dat)))]
+  if(is.null(dim(fusion))) {
+    fusion <- as.data.frame(fusion)
+    rownames(fusion) <- rownames(gen.dat)
+  }
+  colnames(fusion) <- gsub(".fus","",colnames(gen.dat)[na.omit(match(paste0(genes,".fus"),colnames(gen.dat)))])
+  for(i in 1:nrow(fusion)){
+    for(j in 1:ncol(fusion)){
+      if(fusion[i,j]==1) {
+        if(!is.na(mat[match(colnames(fusion)[j],rownames(mat)),match(rownames(fusion)[i],colnames(mat))])){
+          mat[match(colnames(fusion)[j],rownames(mat)),match(rownames(fusion)[i],colnames(mat))] <-
+            paste0(mat[match(colnames(fusion)[j],rownames(mat)),match(rownames(fusion)[i],colnames(mat))],"FUS;")}
+        else{mat[match(colnames(fusion)[j],rownames(mat)),match(rownames(fusion)[i],colnames(mat))] <- "FUS;"}
+      }
     }
   }
 
+  ### clin ###
+  if(!is.null(clin.dat)){
+    temp <- clin.dat
+    for(x in colnames(clin.dat)){
+      y <- clin.dat[,x]
+
+      # if binary #
+      if(length(unique(y[!is.na(y)])) == 2){
+        mat[match(x,rownames(mat)),] <- ifelse(y==1,"CLIN;",NA)
+      }
+
+      else if(length(unique(y[!is.na(y)])) %in% c(3:5)){
+        mat[match(x,rownames(mat)),] <- paste0(abbreviate(x,1),y,";")
+      }
+
+      else{
+        mat[match(x,rownames(mat)),] <- ifelse(y > median(y),"CLIN;",NA)
+      }
+    }
+  }
   ###
   mat[is.na(mat)] <- "  "
   mat <- as.matrix(mat)
---
+++ R/plot-oncoprint.R
@@ -0,0 +1,52 @@
+#' plot.oncoPrint
+#'
+#' Creates the OncoPrint corresponding to the inputted genetic data
+#'
+#' @param gen.dat A binary matrix or dataframe, with patients as rows and features as columns. Note that the names of the
+#' columns must end in ".Del" or ".Amp" to recognize copy number alterations. (see create.bin.matrix for more details on this format).
+#' @param clin.dat An optional clinical file, including only the features the user wishes to add to the plot. Default is NULL.
+#' @param ordered An optional vector of length equal to the number of patients under consideration. Indicates the new order (from left to right)
+#' to be plotted.
+#' @return p : an oncoprint object
+#' @export
+#' @import ComplexHeatmap
+
+
+plot.oncoPrint <- function(gen.dat,clin.dat=NULL,ordered=NULL){
+
+  # make data #
+  mat <- dat.oncoPrint(gen.dat,clin.dat)
+  #############
+  col = c("MUT" = "#008000", "AMP" = "red", "DEL" = "blue", "FUS" = "orange", "CLIN" = "black")
+  alter_fun = list(
+    background = function(x, y, w, h) {
+      grid.rect(x, y, w-unit(0.5, "mm"), h-unit(0.5, "mm"), gp = gpar(fill = "#CCCCCC", col = NA))
+    },
+    DEL = function(x, y, w, h) {
+      grid.rect(x, y, w-unit(0.5, "mm"), h-unit(0.5, "mm"), gp = gpar(fill = "blue", col = NA))
+    },
+    AMP = function(x, y, w, h) {
+      grid.rect(x, y, w-unit(0.5, "mm"), h-unit(0.5, "mm"), gp = gpar(fill = "red", col = NA))
+    },
+    MUT = function(x, y, w, h) {
+      grid.rect(x, y, w-unit(0.5, "mm"), h*0.33, gp = gpar(fill = "#008000", col = NA))
+    },
+    FUS = function(x, y, w, h) {
+      grid.rect(x, y, w-unit(0.5, "mm"), h*0.33, gp = gpar(fill = "orange", col = NA))
+    },
+    CLIN = function(x, y, w, h) {
+      grid.rect(x, y, w-unit(0.5, "mm"), h*0.33, gp = gpar(fill = "black", col = NA))
+    }
+  )
+
+
+  if(!is.null(ordered)) sorted.mat <- mat[,ordered]
+  else sorted.mat <- mat
+  p <- oncoPrint(sorted.mat, get_type = function(x) strsplit(x, ";")[[1]],
+                 alter_fun = alter_fun, col = col, column_order = NULL,row_order = NULL,
+                 # column_title = "OncoPrint for Meso",
+                 barplot_ignore = c("CLIN"),
+                 heatmap_legend_param = list(title = "Alternations", at = c("MUT","DEL","AMP","FUS","CLIN"),
+                                             labels = c("Mutation","Deletion","Amplification","Fusion","ClinicalMarker")))
+  return(p)
+}
