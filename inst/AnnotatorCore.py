a437a677b78d52fd2734cbfca116f1d3488695b1
Axel Martin <axel.steph.martin@gmail.com>
2019-09-10 15:08:51 -0400

    this is iiiiiit

--- inst/AnnotatorCore.py
+++ inst/AnnotatorCore.py
@@ -110,7 +110,7 @@ def gethotspots(url, type):
 missensesinglehotspots = None
 indelsinglehotspots = None
 _3dhotspots = None
-curatedgenes = getcuratedgenes(os.path.join(os.path.dirname(__file__), 'data/curated_genes.txt'))
+curatedgenes = getcuratedgenes(os.path.join(os.path.dirname(__file__), 'curated_genes.txt'))
 
 def inithotspots():
     global missensesinglehotspots
