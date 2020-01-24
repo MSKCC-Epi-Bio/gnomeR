a79d5fcbf29fa9371d7b07ecdde725ff93a37f12
Axel Martin <axel.ste.martin@gmail.com>
2020-01-24 16:47:07 -0500

    Update gen-tab.R

--- R/gen-tab.R
+++ R/gen-tab.R
@@ -46,8 +46,8 @@ gen.tab <- function(gen.dat,outcome,filter=0,paired = F){
     }
     if(paired == F) test <- fisher.test(x, outcome)
     if(paired == T){
-      tt = with(as.data.frame(cbind(x,outcome)), table(x,outcome))
-      test <- exact2x2::mcnemar.exact(tt)
+      # tt = with(as.data.frame(cbind(x,outcome)), table(x,outcome))
+      test <- exact2x2::mcnemar.exact(x[1:length(x)/2],x[(length(x)+1):length(x)])
     }
     out <- c(sum(x)/length(x))
     for (i in 1:length(levels(outcome))) {
