e6537d9689485682f76c732d098bb70ef17d7d10
Axel Martin <axel.ste.martin@gmail.com>
2020-02-04 17:19:17 -0500

    adding plotly and normal outcome for gene tab

--- R/gen-tab.R
+++ R/gen-tab.R
@@ -10,6 +10,7 @@
 #' having a genetic event (only for binary features). All features with an event rate lower than that value will be removed.
 #' Default is 0 (all features included).
 #' @param paired Boolean if the data are paired. Default is FALSE.
+#' @param cont Should the outcome be treated as a continuous value. Default is FALSE treated as categorical.
 #' @return fits : a table of odds ratio and pvalues.
 #' @return forest.plot : A forest plot of the top 10 hits.
 #'
@@ -24,7 +25,7 @@
 #' head(test$fits)
 #' test$forest.plot
 
-gen.tab <- function(gen.dat,outcome,filter=0,paired = F){
+gen.tab <- function(gen.dat,outcome,filter=0,paired = F,cont=F){
 
   # remove all columns that are constant #
   if(length(which(apply(gen.dat, 2, function(x){length(unique(x)) == 1}))) > 0)
@@ -45,58 +46,80 @@ gen.tab <- function(gen.dat,outcome,filter=0,paired = F){
     stop("Only one or fewer genes are left after filtering. We need a minimum of two. Please relax the filter argument.")
 
 
-  fits <- as.data.frame(t(apply(gen.dat, 2, function(x) {
-    if (length(unique(x))/length(x) > 0.5) {
-      x <- ifelse(x > median(x, na.rm = T), 1, 0)
-    }
-    if(paired == F) test <- fisher.test(x, outcome)
-    if(paired == T){
-      # tt = with(as.data.frame(cbind(x,outcome)), table(x,outcome))
-      test <- exact2x2::mcnemar.exact(x = x[1:(length(x)/2)],y = x[(length(x)/2+1):length(x)])
-    }
-    out <- c(sum(x)/length(x))
-    for (i in 1:length(levels(outcome))) {
-      out <- c(out, sum(x[which(outcome == levels(outcome)[i])])/length(which(outcome == levels(outcome)[i])))
-    }
-    out <- paste0(round(as.numeric(out)*100,digits = 2),"%")
-    if (!is.null(test$estimate)) {
-      out <- c(out, as.numeric(test$estimate), test$p.value, round(as.numeric(test$conf.int),digits =2))
-      names(out) <- c("Overall",levels(outcome)[1:length(levels(outcome))],
-                      "OddsRatio", "Pvalue", "Lower", "Upper")
-    }
-    else {
-      out <- c(out, test$p.value, round(as.numeric(test$conf.int),digits =2))
-      names(out) <- c("Overall",levels(outcome)[1:length(levels(outcome))],
-                      "Pvalue")
-    }
-    return(out)
-  })))
+  if(!cont){
+    fits <- as.data.frame(t(apply(gen.dat, 2, function(x) {
+      if (length(unique(x))/length(x) > 0.5) {
+        x <- ifelse(x > median(x, na.rm = T), 1, 0)
+      }
+      if(paired == F) test <- fisher.test(x, outcome)
+      if(paired == T){
+        # tt = with(as.data.frame(cbind(x,outcome)), table(x,outcome))
+        test <- exact2x2::mcnemar.exact(x = x[1:(length(x)/2)],y = x[(length(x)/2+1):length(x)])
+      }
+      out <- c(sum(x)/length(x))
+      for (i in 1:length(levels(outcome))) {
+        out <- c(out, sum(x[which(outcome == levels(outcome)[i])])/length(which(outcome == levels(outcome)[i])))
+      }
+      out <- paste0(round(as.numeric(out)*100,digits = 2),"%")
+      if (!is.null(test$estimate)) {
+        out <- c(out, as.numeric(test$estimate), test$p.value, round(as.numeric(test$conf.int),digits =2))
+        names(out) <- c("Overall",levels(outcome)[1:length(levels(outcome))],
+                        "OddsRatio", "Pvalue", "Lower", "Upper")
+      }
+      else {
+        out <- c(out, test$p.value, round(as.numeric(test$conf.int),digits =2))
+        names(out) <- c("Overall",levels(outcome)[1:length(levels(outcome))],
+                        "Pvalue")
+      }
+      return(out)
+    })))
 
-  fits$FDR <- p.adjust(fits$Pvalue,method="fdr")
-  fits <- fits[order(fits$Pvalue),]
+    fits$FDR <- p.adjust(fits$Pvalue,method="fdr")
+    fits <- fits[order(fits$Pvalue),]
 
-  if (!is.null(fits$OddsRatio)){
-  # forest plot #
-  f.dat <- fits
-  f.dat$Gene <- rownames(f.dat)
-  f.dat <- f.dat %>%
-    filter(!is.infinite(OddsRatio)) %>%
-    mutate(
-      OddsRatio = as.numeric(as.character(OddsRatio)),
-      Lower = as.numeric(as.character(Lower)),
-      Upper = as.numeric(as.character(Upper)),
-    )
-  f.dat <- f.dat[1:min(10,nrow(f.dat)),]
-  forest.plot <- f.dat %>%
-    ggplot(aes(x = Gene,y = OddsRatio, ymin = Lower, ymax = Upper ))+
-    geom_pointrange(aes(col=Gene))+
-    geom_hline(aes(fill=Gene),yintercept =1, linetype=2)+
-    xlab('Gene')+ ylab("Risk Ratio (95% Confidence Interval)")+
-    geom_errorbar(aes(ymin=Lower, ymax=Upper,col=Gene),width=0.5,cex=1)+
-    coord_flip()
+    if (!is.null(fits$OddsRatio)){
+      # forest plot #
+      f.dat <- fits
+      f.dat$Gene <- rownames(f.dat)
+      f.dat <- f.dat %>%
+        filter(!is.infinite(OddsRatio)) %>%
+        mutate(
+          OddsRatio = as.numeric(as.character(OddsRatio)),
+          Lower = as.numeric(as.character(Lower)),
+          Upper = as.numeric(as.character(Upper)),
+        )
+      f.dat <- f.dat[1:min(10,nrow(f.dat)),]
+      forest.plot <- f.dat %>%
+        ggplot(aes(x = Gene,y = OddsRatio, ymin = Lower, ymax = Upper ))+
+        geom_pointrange(aes(col=Gene))+
+        geom_hline(aes(fill=Gene),yintercept =1, linetype=2)+
+        xlab('Gene')+ ylab("Risk Ratio (95% Confidence Interval)")+
+        geom_errorbar(aes(ymin=Lower, ymax=Upper,col=Gene),width=0.5,cex=1)+
+        coord_flip()
+    }
+    else{
+      forest.plot <- NULL
+    }
+    return(list("fits"=fits,"forest.plot"=forest.plot))
   }
-  else{
-    forest.plot <- NULL
+
+
+  ############
+  if(cont){
+    fits <- as.data.frame(do.call('rbind',apply(gen.dat, 2, function(x) {
+      fit <- lm(outcome ~ x)
+      temp <- as.data.frame(summary(fit)$coefficient)
+      colnames(temp) <-  c("Estimate","SD", "tvalue","Pvalue")
+      temp$MutationFreq <- sum(x,na.rm = T)/length(x[!is.na(x)])
+      out <- temp[2,c(1,2,4,5)]
+    })))
+    fits$FDR <- p.adjust(fits$Pvalue,method = "fdr")
+
+    vPlot <- plot_ly(data = fits, x = ~Estimate, y = ~-log10(Pvalue),
+                              text = ~paste('Gene :',GeneName,
+                                            '</br> Estimate :',round(Estimate,digits=2)),
+                              mode = "markers",size = ~MutationFreq,color = ~Estimate) %>%
+      layout(title ="Volcano Plot")
+    return(list("fits"=fits,"vPlot"=vPlot))
   }
-  return(list("fits"=fits,"forest.plot"=forest.plot))
 }
