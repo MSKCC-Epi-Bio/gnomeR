d1ac1896e7582749480ac478f0f7c93d6bb84818
Axel Martin <axel.ste.martin@gmail.com>
2020-02-06 16:16:34 -0500

    Update make-bin-mat.R

--- R/make-bin-mat.R
+++ R/make-bin-mat.R
@@ -133,7 +133,7 @@ create.bin.matrix <- function(patients=NULL, maf, mut.type = "SOMATIC",SNP.only 
     temp <- do.call("cbind",apply(cna,2,function(x){
       if(cna.relax){
         yA <- ifelse(x>=0.9,1,0)
-        yD <- ifelse(x<=0.9,1,0)
+        yD <- ifelse(x<=-0.9,1,0)
       }
       if(!cna.relax){
         yA <- ifelse(x==2,1,0)
