99821eae938250fdd4bbe354b55fd153016e59b6
Axel Martin <axel.steph.martin@gmail.com>
2020-02-05 20:27:47 -0500

    Update facet-heatmap.R

--- R/facet-heatmap.R
+++ R/facet-heatmap.R
@@ -94,13 +94,13 @@ facets.heatmap <- function(seg = NULL,filenames = NULL, path, patients=NULL, min
   if(!is.null(seg)){
     dat <- facets.dat(seg,patients = patients)
     reducedM <- dat$out.cn
-    if(!is.null(ordered) && !is.null(outcome)) {
-      if(length(-which(is.na(match(patients,rownames(reducedM))))) > 0){
-        outcome <- outcome[-which(is.na(match(patients,rownames(reducedM))))]
-        ordered <- order(outcome)
-        outcome <- outcome[ordered]
-      }
-    }
+    # if(!is.null(ordered) && !is.null(outcome)) {
+    #   if(length(-which(is.na(match(patients,rownames(reducedM))))) > 0){
+    #     outcome <- outcome[-which(is.na(match(patients,rownames(reducedM))))]
+    #     ordered <- order(outcome)
+    #     outcome <- outcome[ordered]
+    #   }
+    # }
     rownames(reducedM) <- abbreviate(rownames(reducedM),minlength = 10)
     imagedata=reducedM
     imagedata[imagedata>1.5]=1.5
@@ -138,10 +138,10 @@ facets.heatmap <- function(seg = NULL,filenames = NULL, path, patients=NULL, min
 
     n <- nrow(reducedM)
 
-    if(!is.null(outcome)) x.lab <- outcome
+    if(!is.null(outcome)) x.lab <- outcome[cl$order]
     else x.lab <- rep(1,n)
     # if(!is.null(ordered)) x.lab <- x.lab[ordered]
-    if(is.null(ordered)) x.lab <- x.lab[cl$order]
+    # if(is.null(ordered)) x.lab <- x.lab[cl$order]
 
     scales = list(x = list(at=1:n,labels=x.lab,rot=90),
                   y = list(at = len - chrom.mids, labels = names(table(chr))),
