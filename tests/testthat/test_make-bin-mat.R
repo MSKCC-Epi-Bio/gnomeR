98eaee149193532506fb7a0cca686b27186de146
arorarshi <arshiaurora@gmail.com>
2020-04-02 17:39:52 -0400

    add make-bin-mat tets

---
+++ tests/testthat/test_make-bin-mat.R
@@ -0,0 +1,27 @@
+context("check make bin mat")
+
+test_that("missing column error",{
+
+  mat = matrix(rnorm(1,100), ncol=4)
+  colnames(mat) = c("Hugo_Symbol", "Variant_Classification", "A","B")
+  expect_error(binmat(maf =mat ))
+
+})
+
+test_that("missing column warning",{
+
+  mat = matrix(rnorm(1,100), ncol=4)
+  colnames(mat) = c("Hugo_Symbol", "Variant_Classification", "Tumor_Sample_Barcode","B")
+  expect_error(binmat(maf =mat ))
+
+})
+
+test_that("read in 1000 patients with spe.plat", {
+  set.seed(123)
+  patients <- as.character(unique(mut$Tumor_Sample_Barcode))[sample(1:length(unique(mut$Tumor_Sample_Barcode)), 1000, replace=FALSE)]
+  bin.mut <- binmat(patients = patients,maf = mut,SNP.only = F,include.silent = F, spe.plat = T, rm.empty = FALSE)
+  expect_equal(names(table(colSums(is.na(bin.mut))))[2], "230")
+  expect_equivalent(table(bin.mut[,"TP53"])[2], 454 )
+})
+
+
