0498356807dcfcc2a6877682dee43deeaddedc78
karissawhiting <whitingk@mskcc.org>
2022-04-08 16:42:48 -0400

    update facets and tests

--- tests/testthat/test-plots.R
+++ tests/testthat/test-plots.R
@@ -3,13 +3,25 @@
 
 
 # maf_viz --------
-maf_viz
 
 test_that("Plot layers match expectations",{
     samples <- as.character(unique(gnomeR::mut$Tumor_Sample_Barcode))[1:200]
     all_plots <- maf_viz(maf = mut %>% filter(Tumor_Sample_Barcode %in% samples))
-    expect_equal(names(all_plots), c("varclass", "vartype", "snvclass", "samplevar", "topgenes"))
+    expect_equal(names(all_plots), c("varclass",
+                                     "vartype",
+                                     "snvclass",
+                                     "samplevar",
+                                     "topgenes",
+                                     "genecor",
+                                     "genecomut"))}
+)
 
-  })
 
 
+# add.perc --------
+test_that("test add_perc utility", {
+
+  expect_error(ggvarclass(mut) + add.perc(), NA)
+}
+
+)
