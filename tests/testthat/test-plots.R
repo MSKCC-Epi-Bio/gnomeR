f34618b9e8aa7605633393c24dcf6a0ff6c28bfb
karissawhiting <whitingk@mskcc.org>
2022-04-08 14:24:32 -0400

    add plot tests

--- tests/testthat/test-plots.R
+++ tests/testthat/test-plots.R
@@ -1,9 +1,15 @@
 
 # Test Plotting Functions-----------------------------------------------------------
 
-# General tests ---
-test_that("", {
-  expect_true(TRUE)
 
-})
+# maf_viz --------
+maf_viz
+
+test_that("Plot layers match expectations",{
+    samples <- as.character(unique(gnomeR::mut$Tumor_Sample_Barcode))[1:200]
+    all_plots <- maf_viz(maf = mut %>% filter(Tumor_Sample_Barcode %in% samples))
+    expect_equal(names(all_plots), c("varclass", "vartype", "snvclass", "samplevar", "topgenes"))
+
+  })
+
 
