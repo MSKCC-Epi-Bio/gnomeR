e4bc9f146d5db26f27cf531c5bc9418e7688786b
carokos <cekostrzewa@gmail.com>
2022-04-08 12:53:31 -0400

    Update pathways.R

--- data-raw/pathways.R
+++ data-raw/pathways.R
@@ -2,8 +2,6 @@ library(tidyverse)
 
 pathways <- read_csv(here::here("data-raw", "pathways.csv"))
 
-# pathways$genes <- map(pathways$genes, ~unlist(strsplit(.x, split=", ")))
-
 pathways <- map(pathways, ~ unique(.x[!is.na(.x)]))
 
 usethis::use_data(pathways, overwrite = TRUE)
