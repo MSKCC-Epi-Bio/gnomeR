6f3e5b2f3a6e3293dffb28cbde28b73f7763fcb4
Hannah Fuchs <100386516+hfuchs5@users.noreply.github.com>
2022-10-31 13:42:42 -0400

    internal recode cna alt func

--- R/utils.R
+++ R/utils.R
@@ -268,7 +268,65 @@ annotate_specific_panel <- function(gene_binary,
 
 }
 
+#' Internal function to recode numeric CNA alteration values to factor values
+#'
+#' @param cna a maf (long) form data set of CNAs. Must include an alteration column.
+#'
+#' @return a recoded CNA data set with factor alteration values
+#'
+#'
 
+
+.recode_cna_alterations <- function(cna){
+
+  if("alteration" %in% colnames(cna)){
+    if(is.numeric(cna$alteration)){
+      cna <- cna %>%
+        mutate(alteration = as.character(alteration))
+    }
+
+    if(is.character(cna$alteration)) {
+      #assess levels of alteration
+      levels_in_data <- names(table(cna$alteration))
+
+      allowed_chr_levels <- c(
+        "neutral" = "0",
+        "homozygous deletion" = "-2",
+        "loh" = "-1.5", #this is a placeholder until methods cleared up
+        "hemizygous deletion" = "-1",
+        "gain" = "1",
+        "high level amplification" = "2"
+      )
+
+      #pull any numbers not in allowed list out
+      all_allowed <- c(allowed_chr_levels, names(allowed_chr_levels))
+      not_allowed <- levels_in_data[!levels_in_data %in% all_allowed]
+
+      #abort if unknown values exist
+      if(length(not_allowed) > 0) {
+        cli::cli_abort(c("Unknown values in {.field alteration} field: {.val {not_allowed}}",
+                         "Must be one of the following: {.val {all_allowed}}"))
+      }
+
+      # recode the alteration varaible as factor with those levels
+      # and suppress warnings on this
+      suppressWarnings(
+        cna <- cna %>%
+          mutate(alteration = forcats::fct_recode(.data$alteration, !!!allowed_chr_levels))
+      )
+
+    }
+  }else{
+    cli::cli_abort(c("Data set must contain a column named alteration.",
+                     "If data set is in long form, please put in wide."))
+  }
+
+
+
+
+
+  return(cna)
+}
 
 
 
---
+++ tests/testthat/test-.recode_cna_alterations.R
@@ -0,0 +1,53 @@
+cna <- rename_columns(gnomeR::cna)
+
+cna_no_alt_long <- cna %>%
+  select(-alteration)
+
+not_allowed_nums <- cna %>%
+  mutate(alteration = 3)
+
+mixed_cna_labels <- cna %>%
+  select(-sample_id)%>% #changing alteration values so drop patient ID
+  mutate(alteration = c(1, rep("neutral", nrow(cna) - 1)))
+
+
+test_that("test no error thrown with example dataset", {
+  expect_error(.recode_cna_alterations(cna), NA)
+
+  alt_types <- names(table(.recode_cna_alterations(cna)$alteration))
+  expect_true("high level amplification" %in% alt_types)
+  expect_true("homozygous deletion" %in% alt_types)
+  expect_equal(length(alt_types), 2)
+})
+
+
+test_that("test what happens when api (wide) cna given", {
+  expect_error(.recode_cna_alterations(reformat_cna(cna)))
+})
+
+
+test_that("test that missing alteration throws error", {
+  expect_error(.recode_cna_alterations(cna_no_alt_long))
+})
+
+test_that("test that numbers < -2 or > 2 throw error", {
+  expect_error(.recode_cna_alterations(not_allowed_nums))
+})
+
+test_that("allowed to have numbers and characters in alteration column", {
+  expect_error(.recode_cna_alterations(mixed_cna_labels), NA)
+
+  mixed_cna_labels <- .recode_cna_alterations(mixed_cna_labels)
+
+  count_alterations <- mixed_cna_labels %>%
+    group_by(alteration)%>%
+    summarize(count = n())%>%
+    as.data.frame()
+
+  types_alterations <- names(table(mixed_cna_labels$alteration))
+
+
+  expect_equal(types_alterations, c("gain", "neutral"))
+  expect_equal(count_alterations[1, 2], 1) #only one gain
+  expect_equal(count_alterations[2, 2], nrow(mixed_cna_labels) - 1) #all others neutral
+})
