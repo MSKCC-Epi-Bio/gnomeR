381d055c393ddf7ad87c9c394749e269d37bdb92
Hannah Fuchs <100386516+hfuchs5@users.noreply.github.com>
2022-11-15 14:44:23 -0500

    test 3 types of alt with specify_panel

--- tests/testthat/test-specify-panel.R
+++ tests/testthat/test-specify-panel.R
@@ -1,7 +1,45 @@
-# test specify_platform ----
+# test specify_impact_panels() ----
 
-test_that("test", {
-
-  #example test
-  expect_equal(TRUE, TRUE)
+test_that("gene binary with muts with impact specified", {
+  samples <- gnomeR::cna$sampleId[1:10]
+  bin_impact<-  create_gene_binary(samples=samples,
+                                           mutation = gnomeR::mutations,
+                                           specify_panel = "impact")
+  expect_true(ncol(bin_impact) > 0)
+  expect_true(nrow(bin_impact) > 1)
 })
+
+test_that("gene binary test cna with impact specified", {
+  samples <- gnomeR::cna$sampleId[1:10]
+  bin_impact<-  create_gene_binary(samples=samples,
+                                   cna = gnomeR::cna,
+                                   specify_panel = "impact")
+  expect_true(ncol(bin_impact) > 0)
+  expect_true(nrow(bin_impact) > 1)
+})
+
+# test_that("gene binary test fusion with impact specified", {
+#   samples <- gnomeR::sv$sampleId[20:30]
+#   bin_impact<-  create_gene_binary(samples=samples,
+#                                    fusion = gnomeR::sv,
+#                                    specify_panel = "impact")
+#   expect_true(ncol(bin_impact) > 0)
+#   expect_true(nrow(bin_impact) > 1)
+# })
+
+test_that("gene binary with all three types of alt and impact only",{
+  samples <- gnomeR::mutations$sampleId[gnomeR::mutations$sampleId %in%
+                       gnomeR::cna$sampleId]
+  samples <-  samples[samples %in% gnomeR::sv$sampleId]
+  samples <- samples[1:5]
+  bin_impact<-  create_gene_binary(samples=samples,
+                                   mutation = gnomeR::mutations,
+                                   cna = gnomeR::cna,
+                                   fusion = gnomeR::sv)
+  expect_true(ncol(bin_impact) > 0)
+  expect_true(nrow(bin_impact) > 1)
+})
+
+test_that("test 0 impact genes", {
+})
+
