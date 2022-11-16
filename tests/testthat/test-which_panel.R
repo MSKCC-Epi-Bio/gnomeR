c86e7d1e88cb5e6b4da87397fdffd39301843f16
Hannah Fuchs <100386516+hfuchs5@users.noreply.github.com>
2022-11-16 14:45:31 -0500

    add non-impact test

--- tests/testthat/test-which_panel.R
+++ tests/testthat/test-which_panel.R
@@ -6,6 +6,11 @@ test_that("test one gene only in IMPACT505", {
   expect_equal(ncol(test), 5)
   expect_equal(nrow(test), length(hugo_symbol))
 
+  expect_equal(test$IMPACT505, "yes")
+  expect_equal(test$IMPACT468, "no")
+  expect_equal(test$IMPACT410, "no")
+  expect_equal(test$IMPACT341, "no")
+
 })
 
 test_that("test two genes gene only in IMPACT505", {
@@ -14,5 +19,24 @@ test_that("test two genes gene only in IMPACT505", {
 
   expect_equal(ncol(test), 5)
   expect_equal(nrow(test), length(hugo_symbol))
+
+  expect_equal(test$IMPACT505[test$genes_in_panel == "TP53"], "yes")
+  expect_equal(test$IMPACT468[test$genes_in_panel == "TP53"], "yes")
+  expect_equal(test$IMPACT410[test$genes_in_panel == "TP53"], "yes")
+  expect_equal(test$IMPACT341[test$genes_in_panel == "TP53"], "yes")
+
+})
+
+test_that("test non-impact gene", {
+  hugo_symbol <- c("XXX")
+  test <- which_impact_panel(hugo_symbol)
+
+  expect_equal(ncol(test), 5)
+  expect_equal(nrow(test), length(hugo_symbol))
+
+  expect_equal(test$IMPACT505, "no")
+  expect_equal(test$IMPACT468, "no")
+  expect_equal(test$IMPACT410, "no")
+  expect_equal(test$IMPACT341, "no")
 
 })
