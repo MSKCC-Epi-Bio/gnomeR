c4e40c30f01dedc0d1ea6f319ecc2914467b74b9
karissawhiting <whitingk@mskcc.org>
2023-01-10 12:12:49 -0500

    add test for error qhen required cols missing

--- tests/testthat/test-sanitize.R
+++ tests/testthat/test-sanitize.R
@@ -1,4 +1,5 @@
 
+# General Test ----------------------------------------------------------------
 test_that("both sanitize functions run with no errors", {
   expect_error(sanitize_mutation_input(gnomeR::mutations, include_silent = FALSE), NA)
   expect_error(sanitize_mutation_input(gnomeR::mutations, include_silent = TRUE), NA)
@@ -33,4 +34,10 @@ test_that("alterations properly recoded using internal func", {
   expect_equal(table$Freq[table$Var1 == "amplification"], 3)
 })
 
+# Test Required Columns -------------------------------------------------------
+test_that("both sanitize functions run with no errors", {
 
+  mutations <- select(gnomeR::mutations, -"hugoGeneSymbol")
+  expect_error(sanitize_mutation_input(mutations), "The following*")
+
+})
