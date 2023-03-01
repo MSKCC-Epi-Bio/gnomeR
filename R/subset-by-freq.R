1de1ae8caa64cda759d094d4ad5cd35af7115fb6
karissawhiting <whitingk@mskcc.org>
2023-03-01 18:09:38 -0500

    switch to base R apply to speed up

--- R/subset-by-freq.R
+++ R/subset-by-freq.R
@@ -1,17 +1,36 @@
+#' Subset a Binary Matrix By Alteration Frequency Threshold
+#'
+#' @param gene_binary A data frame with a row for each sample and column for each
+#' alteration. Data frame must have a `sample_id` column and columns for each alteration
+#' with values of 0, 1 or NA.
+#' @param t Threshold value between 0 and 1 to subset by. Default is 10% (.1).
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' samples <- unique(gnomeR::mutations$sampleId)
+#'  gene_binary <- create_gene_binary(
+#'    samples = samples, mutation = mutations, cna = cna,
+#'    mut_type = "somatic_only",
+#'    include_silent = FALSE,
+#'    specify_panel = "impact"
+#'  )
+#'gene_binary %>%
+#'  subset_by_frequency()
+#'
+subset_by_frequency <- function(gene_binary, t = .1) {
 
 
+  # Checks ------------------------------------------------------------------
 
-samples <- unique(gnomeR::mutations$sampleId)
-gene_binary <- create_gene_binary(
-  samples = samples, mutation = mutations, cna = cna,
-  mut_type = "somatic_only",
-  include_silent = FALSE,
-  specify_panel = "IMPACT341"
-)
+  # check threshold argument
+  if(!(is.numeric(t) & (t >= 0 & t <= 1))) {
+    cli::cli_abort("{.field t} must be a number between 0 and 1")
+  }
 
-subset_by_frequency <- function(gene_binary, t = .1 ) {
   if (!is.data.frame(gene_binary)) {
-    cli::cli_abort("{.code gene_binary} must be a data.frame with sample ids")
+    cli::cli_abort("{.code gene_binary} must be a data.frame")
   }
 
   .check_required_cols(gene_binary, "sample_id", "gene_binary")
@@ -19,15 +38,16 @@ subset_by_frequency <- function(gene_binary, t = .1 ) {
   alt_only <- select(gene_binary, -sample_id)
 
   # Remove all NA columns ----------------------------------------------
-  all_na_alt <- purrr::map_lgl(alt_only, function(x) {
+  all_na_alt <- apply(alt_only,  2, function(x) {
     sum(is.na(x)) == nrow(alt_only)
-    })
+  })
+
   all_non_na_alt <- names(all_na_alt[!all_na_alt])
   alt_only <- select(alt_only, all_of(all_non_na_alt))
 
 
-# Check Numeric Class -----------------------------------------------------
-  is_numeric <- purrr::map_lgl(alt_only, ~is.numeric(.x))
+  # Check Numeric Class -----------------------------------------------------
+  is_numeric <- apply(alt_only, 2, is.numeric)
   is_numeric[!is_numeric]
 
   if(!(all(is_numeric))) {
@@ -36,9 +56,17 @@ subset_by_frequency <- function(gene_binary, t = .1 ) {
   }
 
 
-# Calc Frequency ----------------------------------------------------------
-  counts <- apply(alt_only, 2, sum)
-  num_na <- apply(alt_only, 2, function(x) sum(!is.na(x)))
+  # Calc Frequency ----------------------------------------------------------
+  counts <- apply(alt_only, 2,  function(x) {sum(x, na.rm = TRUE)})
+  num_non_na <- apply(alt_only, 2, function(x) sum(!is.na(x)))
 
+  alt_freq <- counts/num_non_na
+
+  alts_over_thresh <- names(alt_freq[alt_freq >= t])
+
+  subset_binary <- select(gene_binary, sample_id,
+                          all_of(alts_over_thresh))
+
+  return(subset_binary)
 
 }
