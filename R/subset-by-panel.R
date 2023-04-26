2f69b8463ec4e54c67680dc71b60169cbaf9dd3e
jalavery <laveryj@mskcc.org>
2023-04-26 14:35:03 -0400

    Add function subset_by_panel and unit tests

---
+++ R/subset-by-panel.R
@@ -0,0 +1,95 @@
+#' Subset a Binary Matrix By Genes Available on Specified Panel
+#'
+#' @param gene_binary A data frame with a row for each sample and column for each
+#' alteration. Data frame must have a `sample_id` column and columns for each alteration
+#' with values of 0, 1 or NA.
+#' @param panel_id A character string or vector of the specified panel to subset the genes (see `gnomeR::gene_panels` for available panels)
+#' @param other_vars One or more column names (quoted or unquoted) in data to be retained
+#' in resulting data frame. Default is NULL.
+#' @return a data frame with a `sample_id` column and columns for
+#' alterations on genes that were sequenced on the specified panel.
+#'
+#' @export
+#'
+#' @examples
+#' samples <- unique(gnomeR::mutations$sampleId)
+#' gene_binary <- create_gene_binary(
+#'   samples = samples, mutation = mutations, cna = cna,
+#'   mut_type = "somatic_only",
+#'   include_silent = FALSE,
+#'   specify_panel = "impact"
+#' )
+#' subset_by_panel(gene_binary = gene_binary, panel_id = "IMPACT468")
+
+# get names of genes that were on the panel but that were unaltered in this sample
+#' setdiff(unnest(gnomeR::gene_panels,
+#'                cols = c("genes_in_panel")) %>%
+#'           filter(gene_panel == "IMPACT300") %>%
+#'           pull(genes_in_panel), names(gene_binary))
+
+subset_by_panel <- function(gene_binary, panel_id, other_vars = NULL){
+  # Checks ------------------------------------------------------------------
+  # check inputs
+  if (!is.data.frame(gene_binary)) {
+    cli::cli_abort("{.code gene_binary} must be a data.frame")
+  }
+
+  .check_required_cols(gene_binary, "sample_id", "gene_binary")
+
+  if (!(panel_id %in% c(gene_panels$gene_panel))){
+    cli::cli_abort("The panel {panel_id} is not an available panel. See `gnomeR::gene_panels()` for the names of available panels.")
+  }
+
+  # Capture Other Columns to Retain -----------------------------------
+  other_vars <-
+    .select_to_varnames({{ other_vars }},
+                        data = gene_binary,
+                        arg_name = "other_vars"
+    )
+
+  # data frame of only alterations
+  alt_only <- select(gene_binary, -"sample_id", -any_of(other_vars))
+
+  # Remove all NA columns ----------------------------------------------
+  all_na_alt <- apply(alt_only,  2, function(x) {
+    sum(is.na(x)) == nrow(alt_only)
+  })
+
+  all_non_na_alt <- names(all_na_alt[!all_na_alt])
+  alt_only <- select(alt_only, all_of(all_non_na_alt))
+
+  # Check Numeric Class -----------------------------------------------------
+  is_numeric <- apply(alt_only, 2, is.numeric)
+
+  if(!(all(is_numeric))) {
+    cli::cli_abort("All alterations in your gene binary must be numeric and only can have values of 0, 1, or NA.
+                   Please coerce the following columns to numeric, or pass them to the `other_vars` argument before proceeding: {.field {names(is_numeric[!is_numeric])}}")
+  }
+
+  # Check Numeric Class -----------------------------------------------------
+  is_numeric <- apply(alt_only, 2, is.numeric)
+
+  if(!(all(is_numeric))) {
+    cli::cli_abort("All alterations in your gene binary must be numeric and only can have values of 0, 1, or NA.
+                   Please coerce the following columns to numeric, or pass them to the `other_vars` argument before proceeding: {.field {names(is_numeric[!is_numeric])}}")
+  }
+
+  # Subset Binary Matrix -----------------------------------------------------
+  # get names of genes on the specified panel
+  genes_on_panel <- gnomeR::gene_panels %>%
+    filter(grepl(panel_id, gene_panel)) %>%
+    unnest("genes_in_panel") %>%
+    pull(genes_in_panel)
+
+  # if not all genes on that panel are included in the genomic binary matrix
+  if (length(setdiff(genes_on_panel, names(gene_binary))) != 0){
+    message(glue::glue("There are {length(setdiff(genes_on_panel, names(gene_binary)))} genes on the {panel_id} panel that are not altered for any patients; columns for those genes are not included in the resulting data frame. To see the names of the genes that are not mutated for any patients in the sample, run `setdiff(unnest(gnomeR::gene_panels, cols = c(\"genes_in_panel\")) %>% filter(gene_panel == panel_id) %>% pull(genes_in_panel), names(gene_binary))`"))
+  }
+
+  # subset to select only those genes
+  subset_binary <- select(gene_binary, "sample_id",
+                          any_of(other_vars),
+                          any_of(genes_on_panel))
+
+  return(subset_binary)
+}
---
+++ tests/testthat/test-subset_by_panel.R
@@ -0,0 +1,109 @@
+
+test_that("Works with binary matrix", {
+
+  bm <- create_gene_binary(mutation = gnomeR::mutations)
+
+  # runs without error
+  expect_no_error(sub <- bm %>%
+                    subset_by_panel(panel_id = "IMPACT300"))
+
+  # expect a message since not all genes sequenced on IMPACT300 are mutated
+  expect_message(subset_by_panel(bm, panel_id = "IMPACT300"))
+})
+
+
+test_that("Error when panel name incorrectly specified", {
+  # panel name not specified
+  expect_error(subset_by_panel(bm))
+
+  # panel name is incorrect
+  expect_error(subset_by_panel(bm, panel_id = "HACKATHON"))
+})
+
+
+test_that("Error when missing sample ID", {
+
+  expect_error(subset_by_panel(bm %>% select(-sample_id), panel_id = "IMPACT300"))
+
+})
+
+
+test_that("Error when non numeric cols", {
+
+  bm_sim <- bind_rows(
+    "gene10" = c(rep(0, 9), rep(1, 1)),
+    "gen50" = c(rep(0, 5), rep(1, 5)),
+    "gene20" = c(rep(0, 8), rep(1, 2)),
+    "gene0" = c(rep(0, 10), rep(1, 0))) %>%
+    mutate(sample_id = as.character(1:nrow(.))) %>%
+    mutate(across(everything(), ~as.character(.x)))
+
+  expect_error(subset_by_panel(panel = "IMPACT300"))
+
+})
+
+test_that("Correctly removes cols with all NA", {
+
+  bm_sim <- bind_rows(
+    "allna" = c(rep(NA, 10)),
+    "ALK" = c(rep(0, 5), rep(1, 5))) %>%
+    mutate(sample_id = as.character(1:nrow(.)))
+
+  sub <- bm_sim %>%
+    subset_by_panel(panel_id = "IMPACT300")
+
+  expect_equal(setdiff(names(bm_sim), names(sub)), "allna")
+
+})
+
+test_that("Other columns are retained in `other_vars`", {
+
+  bm_sim <- bind_rows(
+    "ALK" = c(rep(0, 5), rep(1, 5)),
+    "EGFR" = c(rep(0, 8), rep(1, 2)),
+    "BRCA1" = c(rep(0, 10), rep(1, 0)),
+    "sex" = rep(c("F", "M"), 5),
+    "stage" = rep(c("I", "II"), 5)) %>%
+    mutate(sample_id = as.character(1:nrow(.)))
+
+  sub <- bm_sim %>%
+    select(-sex, -stage) %>%
+    subset_by_frequency(t = 0)
+
+  sub2 <- bm_sim %>%
+    subset_by_panel(panel = "IMPACT300", other_vars = c(sex, stage))
+
+  expect_equal(setdiff(names(sub2), names(sub)), c("sex", "stage"))
+
+
+})
+
+test_that("Pass `other_vars` as strings works", {
+
+  bm_sim <- bind_rows(
+    "ALK" = c(rep(0, 5), rep(1, 5)),
+    "EGFR" = c(rep(0, 8), rep(1, 2)),
+    "BRCA1" = c(rep(0, 10), rep(1, 0)),
+    "sex" = rep(c("F", "M"), 5),
+    "stage" = rep(c("I", "II"), 5)) %>%
+    mutate(sample_id = as.character(1:nrow(.)))
+
+  sub <- bm_sim %>%
+    subset_by_panel(panel_id = "IMPACT300", other_vars = c(sex, stage))
+
+  sub2 <- bm_sim %>%
+    subset_by_panel(panel_id = "IMPACT300", other_vars = c("sex", "stage"))
+
+  expect_equal(names(sub2), names(sub))
+})
+
+test_that("Count of genes not included is correct", {
+  bm_sim <- bind_rows(
+    "ALK" = c(rep(0, 5), rep(1, 5)),
+    "EGFR" = c(rep(0, 8), rep(1, 2)),
+    "BRCA1" = c(rep(0, 10), rep(1, 0))) %>%
+    mutate(sample_id = as.character(1:nrow(.)))
+
+  expect_message(subset_by_panel(bm_sim, panel_id = "IMPACT300"),
+                 "^There are 297")
+})
