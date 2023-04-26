fb89ddb8cfaf8e1f60d8a5485dfc1e5c68a034e3
karissawhiting <whitingk@mskcc.org>
2023-04-25 22:36:12 -0400

    deprecate count_pathways_by arg and associated functionality

--- R/add-pathways.R
+++ R/add-pathways.R
@@ -1,12 +1,16 @@
 #' Pathway Alterations
 #'
-#' Input a binary matrix of patients x alerations and return a dataframe with a column per pathway
+#' Input a binary matrix of patients x alterations and return a dataframe with a column per pathway
 #' indicating if default or custom oncogenic signaling pathways
 #' are activated in each sample. Default package pathways were sourced
 #' from [Sanchez-Vega, F et al., 2018](https://pubmed.ncbi.nlm.nih.gov/29625050/).
 #'
 #' Please check for gene aliases in your data set before using.
 #'
+#'
+#' @description
+#' Please note that only `sample_id column`, and columns with .Amp, .Del, .fus or no suffix are accepted.
+#' Any gene column with no suffix will be assumed to be a mutation.
 #'
 #' @param gene_binary a binary matrix from `create_gene_binary()`
 #' @param pathways a vector of pre-coded pathways to annotate. The options are `names(gnomeR::pathways)` ("RTK/RAS", "Nrf2",
@@ -16,6 +20,7 @@
 #' @param custom_pathways a vector of alterations to annotate as a single pathway, or a list of custom pathways (see `gnomeR::pathways` as example).
 #' You must specify the alteration type for each gene using `.mut`, `.Amp`, `.Del` suffix, e.g. `c("TP53.mut", "CDKN2A.Amp")`. If you wish to count any type of
 #' alteration on that gene towards the pathway you can use the `.any` suffix (e.g. `c("TP53.any")`).
+#' @param count_pathways_by deprecated
 #' @keywords internal
 #' @return a data frame: each sample is a row, columns are pathways, with values of 0/1 depending on pathway alteration status.
 #' @export
@@ -31,21 +36,28 @@
 add_pathways <- function(gene_binary,
                          pathways = c(names(gnomeR::pathways)),
                          custom_pathways = NULL,
-                         count_pathways_by = c("alteration", "gene")) {
-
-  all_path <- gnomeR::pathways
-  all_path_names <- names(all_path)
+                         count_pathways_by = deprecated()) {
 
   # Check arguments -----------------------------------------------------------
 
+  all_path <- gnomeR::pathways
+  all_path_names <- names(all_path)
   .check_required_cols(gene_binary, "sample_id", "gene_binary")
 
-  # Custom pathways ----
-  switch(!(class(custom_pathways) %in% c("NULL", "character", "list")),
-         cli::cli_abort("{.code custom_pathways} must be character vector, or list"))
+  # * Deprecated Arguments (will remove this in the future) ----
+
+  if (lifecycle::is_present(count_pathways_by)) {
+    lifecycle::deprecate_stop(when = "1.3.0",
+                              what = "add_pathways(count_pathways_by)",
+                              details = c(
+                                i = c("All pathways are now counted on the specific alteration level, not the gene level and all columns in your data with no suffix (e.g. `data$TP53`) are assumed to be mutations.
+                                You must explicitely specify in your custom pathway what types of alterations count towards pathway e.g. `custom_pathway = c('TP53.mut', 'APC.Del').
+                                      If you would like to count any type of alteration on a gene towards a pathway, use the suffix `.all`, e.g. `custom_pathway = c('TP53.all', 'APC.all')")))
+  }
 
 
-  # Default pathways ----
+
+  # * Default pathways ----
   pathways_input <- pathways
 
   if(!is.null(pathways)) {
@@ -57,16 +69,12 @@ add_pathways <- function(gene_binary,
   switch(length(not_valid) > 0,
          cli::cli_warn("Ignoring {.code {not_valid}}: not a known pathway. See {.code gnomeR::pathways}"))
 
-  # count pathways by
-  count_pathways_by <- match.arg(count_pathways_by, c("alteration", "gene"))
 
-  all_cols <- colnames(gene_binary)
-  mut_cols <- !(str_detect(all_cols, ".Amp|.Del|.fus|.cna"))
+  # Custom pathways ----
+  switch(!(class(custom_pathways) %in% c("NULL", "character", "list")),
+         cli::cli_abort("{.code custom_pathways} must be character vector, or list"))
 
-  switch(is.null(custom_pathways) & count_pathways_by == "gene",
-         cli::cli_alert_warning("Annotating the default pathways by gene may be inappropriate."))
 
-  # custom_pathways:  can be list or vector------------------------------------
   if (!is.null(custom_pathways)) {
 
     # if vector, turn into a list---
@@ -88,57 +96,52 @@ add_pathways <- function(gene_binary,
       }
     }
 
+    if(any(purrr::map_lgl(custom_pathways, ~any(!str_detect(.x, ".Amp|.Del|.fus|.mut"))))) {
+      cli::cli_abort(c("All alterations specified in {.code custom_pathway} must have one ",
+                      "of the following suffixes specified: {.code .Amp}, {.code .Del}, {.code .fus}, {.code .mut}, {.code .all}"))
+    }
 
-    if(count_pathways_by == "alteration") {
 
-      # add mut on custom pathways when count_pathways_by == "alteration"
-      if(any(purrr::map_lgl(custom_pathways, ~any(!str_detect(.x, ".Amp|.Del|.fus|.cna|.mut"))))) {
-        cli::cli_inform("Assuming any gene in {.code custom_pathway} without
-        suffix {.code .Amp|.Del|.fus|.cna} is specifically a mutation in that pathway. CNA and fusions will not be counted (e.g. TP53.Del). To control this behavior, see argument {.code count_pathways_by}")
+    # process GENE.all suffix in custom pathways
+    custom_pathways <- purrr::map(custom_pathways, function(x) {
+      gene_all <- x[str_detect(x, ".all")]
+
+      if(length(gene_all) > 0) {
+        gene_all <-  str_remove(gene_all, ".all")
+        gene_add <- paste0(gene_all, c(".Amp", ".Del", ".fus", ".mut"))
+        x <- c(x[!str_detect(x, ".all")], gene_add)
       }
 
+      unique(x)
 
-      custom_pathways <- purrr::map(custom_pathways, function(x) {
-        x[!str_detect(x, ".Amp|.Del|.fus|.cna")] <-
-          paste0(stringr::str_trim(
-            x[!str_detect(x, ".Amp|.Del|.fus|.cna")]), ".mut")
-
-        unique(x)
-
-        } )
+      } )
 
     }
-
-    if(count_pathways_by == "gene" &
-       any(purrr::map_lgl(custom_pathways, ~any(str_detect(.x, ".Amp|.Del|.fus|.cna"))))) {
-      cli::cli_warn("You selected {.code count_pathways_by = 'gene'}. Ignoring {.code .Amp|.Del|.fus|.cna} in {.code custom_pathway} genes passed")
-    }
-
-    }
-
 
 
   # get user selected pathways (if any)
   pathways <- all_path[pathways]
   final_paths <- c(custom_pathways, pathways)
 
-  if(count_pathways_by == "gene") {
-    final_paths <- purrr::map(final_paths, ~unique(str_remove_all(.x, ".mut|.Amp|.Del|.fus|.cna|.meth")))
-  }
-
   # prep data ------------------------------------------------------------------
+  all_cols <- colnames(gene_binary)
+  mut_cols <- !(str_detect(all_cols, ".Amp|.Del|.fus"))
 
   # rename mut cols -assume all non CNA/Fusion are mutations
   if (any(mut_cols)) {
+
+    # in case any columns already had .mut suffix
+    all_cols[mut_cols] <- str_remove(all_cols[mut_cols], ".mut")
+
     all_cols[mut_cols] <- paste0(all_cols[mut_cols], ".mut")
+
     colnames(gene_binary) <- all_cols
   }
 
   # process pathways ---------------------------------------------------------------
   path_out <- purrr::imap_dfc(final_paths, ~ .sum_alts_in_pathway(gene_binary = gene_binary,
                                                                  pathway_list_item = .x,
-                                                                 pathway_name = .y,
-                                                                 count_pathways_by = count_pathways_by))
+                                                                 pathway_name = .y))
 
   # return data  ---------------------------------------------------------------
 
@@ -149,7 +152,6 @@ add_pathways <- function(gene_binary,
   }
 
   path_out <- gene_binary %>%
-#    select("sample_id") %>%
     bind_cols(path_out)
 
   return(path_out)
@@ -162,7 +164,6 @@ add_pathways <- function(gene_binary,
 #' @param pathway_list_item a named list of length 1 with pathway name as name and vector of genes as first
 #' and only item in list
 #' @param pathway_name name of pathway
-#' @param count_pathways_by passed from `add_pathways()`
 #'
 #' @return a dataframe of 1 column of 0/1s indicating pathway activated yes/no
 #' @keywords internal
@@ -179,15 +180,8 @@ add_pathways <- function(gene_binary,
 .sum_alts_in_pathway <- function(gene_binary, pathway_list_item,
                                  pathway_name,
                                  count_pathways_by) {
-  path_alt <- switch(count_pathways_by,
-                     "alteration" = {
-                       gene_binary %>%
-                         select(any_of(unlist(pathway_list_item, use.names=FALSE)))
-                     },
-                     "gene" = {
-                       gene_binary %>%
-                         select(contains(unlist(pathway_list_item, use.names=FALSE)))
-                     }) %>%
+  path_alt <- gene_binary %>%
+    select(any_of(unlist(pathway_list_item, use.names=FALSE))) %>%
     mutate(sum = rowSums(., na.rm = TRUE)) %>%
     transmute('pathway_{pathway_name}' := if_else(sum >= 1, 1, 0))
 
--- tests/testthat/test-add_pathways.R
+++ tests/testthat/test-add_pathways.R
@@ -1,3 +1,19 @@
+# Ideas for tests to be added-----------
+#
+# 1. Test that if custom_pathway with no suffix passed it will error e.g. c(TP53, APC)
+# Test the above when list or vector passed to custom_pathway
+# example of list
+# custom_pathways = list(
+#   c("TP53.all", "APC"),
+#   c("RB1.Del", "FGFR3.all"))
+#
+# 2. Test that passing custom pathway with GENE.all would return same as
+# custom pathway with GENE.mut, GENE.Del, GENE.Amp, GENE.fus
+#
+# 3. Test that a binary matrix with columns that have .mut suffix (e.g. column name TP53.mut) will correctly be
+# processed same as columns with no suffix  (e.g. column name TP53)
+#
+# 4. Make sure all works with both a few default `pathways` AND `custom_pathways` passed at the same time
 
 test_that("add_pathways function works with default input", {
 
@@ -44,32 +60,6 @@ test_that("pass specific pathways", {
 })
 
 
-test_that("pass incorrect pathway", {
-
-  gene_binary_ex <- gnomeR::create_gene_binary(mutation = gnomeR::mutations[1:50, ],
-                                               cna = cna,
-                                               fusion = gnomeR::sv[1:10,])
-
-  # create fake ALK column so counting by genes is different than counting by alterations
-  gene_binary_ex$PTEN = gene_binary_ex$AR
-
-  expect_message(cust <- add_pathways(gene_binary = gene_binary_ex,
-                                      custom_pathways = c("AKT1", "PTEN")))
-
-  # check summed only mutations in path
-  expect_equal(sum(cust$pathway_custom),
-               sum(gene_binary_ex$AKT1, gene_binary_ex$PTEN))
-
-  cust2 <- add_pathways(gene_binary = gene_binary_ex,
-                        custom_pathways = c("AKT1", "PTEN"),
-                        count_pathways_by = "gene")
-
-  #check summed only mutations in path and not all
-  expect_true(sum(cust2$pathway_custom) > sum(cust$pathway_custom))
-
-
-})
-
 test_that("vector custom pathway with NULL pathways", {
 
 
@@ -77,9 +67,9 @@ test_that("vector custom pathway with NULL pathways", {
                                                cna = gnomeR::cna,
                                                fusion = gnomeR::sv[1:10,])
 
-  expect_message(cust <- add_pathways(gene_binary = gene_binary_ex,
+  cust <- add_pathways(gene_binary = gene_binary_ex,
                                       pathways = NULL,
-                                      custom_pathways = c("TP53", "APC")))
+                                      custom_pathways = c("TP53.mut", "APC.Del"))
 
   expect_equal(c(names(gene_binary_ex), "pathway_custom"),
                names(cust))
@@ -92,11 +82,11 @@ test_that("list custom pathway with NULL pathways", {
                                                cna = gnomeR::cna,
                                                fusion = gnomeR::sv[1:10,])
 
-  expect_message(cust <- add_pathways(gene_binary = gene_binary_ex,
+  cust <- add_pathways(gene_binary = gene_binary_ex,
                                       pathways = NULL,
                                       custom_pathways = list(
-                                        "paththingyyy" = c("TP53", "APC"),
-                                        "patheroo" = c("RB1.Del"))), "*")
+                                        "paththingyyy" = c("TP53.mut", "APC.mut"),
+                                        "patheroo" = c("RB1.Del")))
 
   expect_equal(c(names(gene_binary_ex), "pathway_paththingyyy", "pathway_patheroo"),
                names(cust))
@@ -109,11 +99,11 @@ test_that("list custom pathway with NULL names", {
                                                cna = gnomeR::cna,
                                                fusion = gnomeR::sv[1:10,])
 
-  expect_message(cust <- add_pathways(gene_binary = gene_binary_ex,
+  cust <- add_pathways(gene_binary = gene_binary_ex,
                                       pathways = NULL,
                                       custom_pathways = list(
-                                        c("TP53", "APC"),
-                                        c("RB1.Del"))), "*")
+                                        c("TP53.mut", "APC.mut"),
+                                        c("RB1.Del")))
 
 
   expect_equal(c(names(gene_binary_ex), "pathway_custom_1", "pathway_custom_2"),
@@ -121,70 +111,59 @@ test_that("list custom pathway with NULL names", {
 
 })
 
-test_that("list custom pathway with NULL names by gene", {
 
-  gene_binary_ex <- gnomeR::create_gene_binary(mutation = gnomeR::mutations[1:10, ],
-                                               cna = gnomeR::cna,
-                                               fusion = gnomeR::sv[1:10,])
+# These are tests of old deprecated arguments. They can be recycled or deleted
 
-  expect_warning(add_pathways(gene_binary = gene_binary_ex,
-               pathways = NULL,
-               count_pathways_by = "gene",
-               custom_pathways = list(
-                 c("TP53", "APC"),
-                 c("RB1.Del"))))
-
-})
-
-# # count_pathways_by ----------------------------------------------------------
-test_that("same results when only mutations passed with count_pathways_by gene or alt ", {
-
-  mut_valid_sample_ids<- unique(gnomeR::mutations$sampleId)[1:10]
-  gene_binary_ex <- create_gene_binary(sample=mut_valid_sample_ids,
-                                       mutation=gnomeR::mutations)
-
-  gene <- add_pathways(gene_binary = gene_binary_ex,
-                       pathways = NULL,
-                       custom_pathways = c("TP53", "APC"),
-                       count_pathways_by = "gene")
-
-  expect_message(alt <- add_pathways(gene_binary = gene_binary_ex,
-                      pathways = NULL,
-                       custom_pathways = c("TP53", "APC"),
-                       count_pathways_by = "alteration"))
-
-  expect_equal(alt$pathway_custom, gene$pathway_custom)
-
-})
-
-test_that("works with count_pathways_by gene or alt ", {
-
-  mut_valid_sample_ids <- c("P-0002375-T01-IM3", "P-0003541-T01-IM5", "P-0005571-T01-IM5")
-  gene_binary_ex <- create_gene_binary(sample=mut_valid_sample_ids,
-                                       mutation=gnomeR::mutations,
-                                       cna = gnomeR::cna)
-
-  gene <- add_pathways(gene_binary = gene_binary_ex,
-                       pathways = NULL,
-                       custom_pathways = c("TP53", "APC"),
-                       count_pathways_by = "gene")
-
-  expect_message(alt <- add_pathways(gene_binary = gene_binary_ex,
-                                     pathways = NULL,
-                                     custom_pathways = c("TP53", "APC"),
-                                     count_pathways_by = "alteration"))
-
-  expect_message(alt2 <- add_pathways(gene_binary = gene_binary_ex,
-                                     pathways = NULL,
-                                     custom_pathways = c("TP53.Del", "APC"),
-                                     count_pathways_by = "alteration"))
-
-  expect_equal(sum(gene$pathway_custom), sum(gene_binary_ex$TP53, gene_binary_ex$TP53.Del))
-  expect_equal(sum(alt$pathway_custom), sum(gene_binary_ex$TP53))
-  expect_equal(sum(alt2$pathway_custom), sum(gene_binary_ex$TP53.Del))
-
-
-})
+# # # count_pathways_by ----------------------------------------------------------
+# test_that("same results when only mutations passed with count_pathways_by gene or alt ", {
+#
+#   mut_valid_sample_ids<- unique(gnomeR::mutations$sampleId)[1:10]
+#   gene_binary_ex <- create_gene_binary(sample=mut_valid_sample_ids,
+#                                        mutation=gnomeR::mutations)
+#
+#   gene <- add_pathways(gene_binary = gene_binary_ex,
+#                        pathways = NULL,
+#                        custom_pathways = c("TP53", "APC"),
+#                        count_pathways_by = "gene")
+#
+#   expect_message(alt <- add_pathways(gene_binary = gene_binary_ex,
+#                       pathways = NULL,
+#                        custom_pathways = c("TP53", "APC"),
+#                        count_pathways_by = "alteration"))
+#
+#   expect_equal(alt$pathway_custom, gene$pathway_custom)
+#
+# })
+#
+# test_that("works with count_pathways_by gene or alt ", {
+#
+#   mut_valid_sample_ids <- c("P-0002375-T01-IM3", "P-0003541-T01-IM5", "P-0005571-T01-IM5")
+#   gene_binary_ex <- create_gene_binary(sample=mut_valid_sample_ids,
+#                                        mutation=gnomeR::mutations,
+#                                        cna = gnomeR::cna)
+#
+#   gene <- add_pathways(gene_binary = gene_binary_ex,
+#                        pathways = NULL,
+#                        custom_pathways = c("TP53", "APC"),
+#                        count_pathways_by = "gene")
+#
+#   expect_message(alt <- add_pathways(gene_binary = gene_binary_ex,
+#                                      pathways = NULL,
+#                                      custom_pathways = c("TP53", "APC"),
+#                                      count_pathways_by = "alteration"))
+#
+#   expect_message(alt2 <- add_pathways(gene_binary = gene_binary_ex,
+#                                      pathways = NULL,
+#                                      custom_pathways = c("TP53.Del", "APC"),
+#                                      count_pathways_by = "alteration"))
+#
+#   expect_equal(sum(gene$pathway_custom), sum(gene_binary_ex$TP53, gene_binary_ex$TP53.Del))
+#   expect_equal(sum(alt$pathway_custom), sum(gene_binary_ex$TP53))
+#   expect_equal(sum(alt2$pathway_custom), sum(gene_binary_ex$TP53.Del))
+#
+#
+# })
+#
 
 
 
