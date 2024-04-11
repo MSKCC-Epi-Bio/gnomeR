adf7828005cf3a0aa92d17d6684315071567b0bf
BrownS7 <samanthalbrown8@gmail.com>
2024-04-11 09:37:59 -0400

    Split out tbl_wide code to its own helper function

--- R/tbl_genomic.R
+++ R/tbl_genomic.R
@@ -1,16 +1,23 @@
 #' tbl_genomic
 #'
-#' This function will select genes based on user inputs or on frequency counts and then
-#' will pass the data.frame to `gtsummary::tbl_summary()`. You can specify a `by` variable and other
-#' parameters that are accepted by `gtsummary::tbl_summary()`. Note the `by` variable must be merged on to
-#' onto the data before using the `by` parameter in the function.
+#' This function will select genes based on user inputs or on frequency counts
+#' and then will pass the data.frame to `gtsummary::tbl_summary()`. You can
+#' specify a `by` variable and other parameters that are accepted by
+#' `gtsummary::tbl_summary()`. Note the `by` variable must be merged on to onto
+#' the data before using the `by` parameter in the function.
 #'
 #' @param gene_binary data.frame of genetic samples
 #' @param freq_cutoff deprecated
 #' @param freq_cutoff_by_gene deprecated
 #' @param gene_subset deprecated
-#' @param by A variable to be passed to `gtsummary::tbl_summary()`'s by parameter
-#' @param ... Additional parameters that can be passed to `gtsummary::tbl_summary()`. To access the additional parameters you need to load `gtsummary`.
+#' @param wide_format Specifies whether to stratify tbl_genomic by alteration
+#'   type such that the resulting table will include one column per alteration
+#'   type and an overall summary column. Default is FALSE.
+#' @param by A variable to be passed to `gtsummary::tbl_summary()`'s by
+#'   parameter
+#' @param ... Additional parameters that can be passed to
+#'   `gtsummary::tbl_summary()`. To access the additional parameters you need to
+#'   load `gtsummary`.
 #' @return A `tbl_summary()` object
 #' @export
 #'
@@ -112,106 +119,11 @@ tbl_genomic <- function(gene_binary,
 
   if (wide_format) {
 
-    genes <- order_genes[!order_genes %in% by]%>%
-      .remove_endings()%>%
-      unique()
-
-    # identify types of alterations in data
-
-    gene_endings <- c(".mut", ".Amp", ".Del", ".fus")
-
-    # add .mut to endings to make easier
-
-    gb_alt_all <- order_genes %>%
-      .paste_endings()
-
-    any_alt_types <- purrr::map(gene_endings,
-                          ~any(grepl(.x, gb_alt_all)))
-
-    names(any_alt_types) <- gene_endings
-
-    # create table of overall frequencies
-
-    tbl1 <- gene_binary %>%
-      pivot_longer(!sample_id)%>%
-      mutate(name = .remove_endings(name))%>%
-      group_by(sample_id, name)%>%
-      slice(which.max(value))%>%
-      ungroup()%>%
-      select(-"sample_id")
-
-
-    tbl2 <- tbl1 %>%
-      split(tbl1$name)
-
-    tbl_overall <- purrr::map(1:length(names(tbl2)), function(x){
-        tbl2[[x]] %>%
-          select(-"name")%>%
-          setNames(names(tbl2)[[x]])
-      })%>%
-      do.call(cbind, .)%>%
-      gtsummary::tbl_summary(by = any_of(by))
-
-    # create table of mutation frequencies
-
-    tbls_alt_types <- purrr::map2(
-  any_alt_types,
-  gene_endings,
-  function(yes_exist, gene) {
-
-    if(yes_exist[[1]]){
-      data <- table_data
-
-      # need to figure out how to add by variables HERE
-      names(data) <- c("sample_id", any_of(by),
-                       .paste_endings(names(data)[2:length(names(data))]))
-
-      data <- data %>%
-        select(any_of(by),
-               ends_with(gene))
-
-      names(data) <- .remove_endings(names(data))
-
-      genes_not_obs <- setdiff(genes, names(data))[setdiff(genes, names(data)) %in% by]
-
-      data <- data %>%
-        # need to fill in 0 for all unobserved alterations in hugo_symbol
-        dplyr::mutate(!!!setNames(rep(0, length(genes_not_obs)),
-                                  genes_not_obs))
-
-      data %>%
-        gtsummary::tbl_summary(by = any_of(by))
-    } else {
-      NULL
-    }
-
-  }
-)
-
-
-    # create list of tables
-    tbls_list_pre <-
-      append(list(tbl_overall),
-           purrr::map2(any_alt_types,
-                       tbls_alt_types, function(x, y){
-             if (x) {y}
-           }))
-
-    # drop NULL tables
-    tbls_list <- tbls_list_pre %>% purrr::keep( ~ !is.null(.) )
-
-
-    tab_spanner_vec <- c(
-      "**Overall**",
-      purrr::map2(any_alt_types, c("**Mutations**", "**Amplifications**",
-                            "**Deletions**", "**Fusions**"),
-           ~if(.x){.y}) %>%
-        unlist()
+    final_table <- .create_wide_format(
+      gene_binary_df = gene_binary,
+      order_genes_df = order_genes,
+      table_data_df = table_data
     )
-
-    # merge tables
-    gtsummary::tbl_merge(tbls_list,
-                         tab_spanner = tab_spanner_vec)
 
   }
 
@@ -228,3 +140,124 @@ tbl_genomic <- function(gene_binary,
   final_table}
 
 }
+
+#' * Wide format by alteration type -----
+#'
+#' This helper function will stratify tbl_genomic by alteration type such that
+#' the resulting table will include one column per alteration type and an
+#' overall summary column.
+#'
+#' @param gene_binary_df
+#' @param order_genes_df
+#' @param table_data_df
+#'
+#' @return wide format 'tbl_genomic' by alteration type
+#' @keywords internal
+#' @export
+#'
+.create_wide_format <- function(gene_binary_df, order_genes_df, table_data_df){
+
+  genes <- order_genes_df[!order_genes_df %in% by]%>%
+    .remove_endings()%>%
+    unique()
+
+  # identify types of alterations in data
+
+  gene_endings <- c(".mut", ".Amp", ".Del", ".fus")
+
+  # add .mut to endings to make easier
+
+  gb_alt_all <- order_genes_df %>%
+    .paste_endings()
+
+  any_alt_types <- purrr::map(gene_endings,
+                              ~any(grepl(.x, gb_alt_all)))
+
+  names(any_alt_types) <- gene_endings
+
+  # create table of overall frequencies
+
+  tbl1 <- gene_binary_df %>%
+    tidyr::pivot_longer(!sample_id)%>%
+    dplyr::mutate(name = .remove_endings(name))%>%
+    dplyr::group_by(sample_id, name)%>%
+    dplyr::slice(which.max(value))%>%
+    dplyr::ungroup()%>%
+    dplyr::select(-"sample_id")
+
+
+  tbl2 <- tbl1 %>%
+    split(tbl1$name)
+
+  tbl_overall <- purrr::map(1:length(names(tbl2)), function(x){
+    tbl2[[x]] %>%
+      dplyr::select(-"name")%>%
+      stats::setNames(names(tbl2)[[x]])
+  })%>%
+    do.call(cbind, .)%>%
+    gtsummary::tbl_summary(by = any_of(by))
+
+  # create table of mutation frequencies
+
+  tbls_alt_types <- purrr::map2(
+    any_alt_types,
+    gene_endings,
+    function(yes_exist, gene) {
+
+      if(yes_exist[[1]]){
+        data <- table_data
+
+        # need to figure out how to add by variables HERE
+        names(data) <- c("sample_id", any_of(by),
+                         .paste_endings(names(data)[2:length(names(data))]))
+
+        data <- data %>%
+          dplyr::select(any_of(by),
+                        ends_with(gene))
+
+        names(data) <- .remove_endings(names(data))
+
+        genes_not_obs <- dplyr::setdiff(genes, names(data))[dplyr::setdiff(genes, names(data)) %in% by]
+
+        data <- data %>%
+          # need to fill in 0 for all unobserved alterations in hugo_symbol
+          dplyr::mutate(!!!stats::setNames(rep(0, length(genes_not_obs)),
+                                           genes_not_obs))
+
+        data %>%
+          gtsummary::tbl_summary(by = any_of(by))
+      } else {
+        NULL
+      }
+
+    }
+  )
+
+  # create list of tables
+  tbls_list_wide_pre <-
+    append(list(tbl_overall),
+           purrr::map2(any_alt_types,
+                       tbls_alt_types, function(x, y){
+                         if (x) {y}
+                       }))
+
+  # drop NULL tables
+  tbls_list_wide <- tbls_list_wide_pre %>% purrr::keep( ~ !is.null(.) )
+
+
+  tab_spanner_vec <- c(
+    "**Overall**",
+    purrr::map2(any_alt_types, c("**Mutations**", "**Amplifications**",
+                                 "**Deletions**", "**Fusions**"),
+                ~if(.x){.y}) %>%
+      unlist()
+  )
+
+  # merge tables
+  final_table_wide <- gtsummary::tbl_merge(tbls_list_wide,
+                                           tab_spanner = tab_spanner_vec)
+
+  return(final_table_wide)
+
+}
+
