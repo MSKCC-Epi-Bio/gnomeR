35555eedea088109098dc1fc7260bc8bed553156
toumban <toumban@mskcc.org>
2024-05-07 12:37:56 -0400

    Add by option to subet by frequency. Function needs to be updated.

--- tests/testthat/test-subset_by_freq.R
+++ tests/testthat/test-subset_by_freq.R
@@ -174,3 +174,77 @@ test_that("Pass `other_vars` as strings works", {
   expect_equal(names(sub2), names(sub))
 })
 
+
+test_that("Check for unknown `by` variable", {
+
+  bm <- bind_rows(
+    "gen50" = c(rep(0, 5), rep(1, 5)),
+    "gene20" = c(rep(0, 8), rep(1, 2)),
+    "gene0" = c(rep(0, 10), rep(1, 0)),
+    "sex" = rep(c("F", "M"), 5),
+    "stage" = rep(c("I", "II"), 5)) %>%
+    mutate(sample_id = as.character(1:nrow(.)))
+
+  expect_error(bm |>
+                 subset_by_frequency(t = .1, other_vars = c(sex, stage), by = grade),
+               "Error in `by=` argument input. Select from")
+
+})
+
+
+test_that("Check `by` variable works", {
+
+  bm <- bind_rows(
+    "gen50" = c(rep(0, 5), rep(1, 5)),
+    "gene20" = c(rep(0, 8), rep(1, 2)),
+    "gene0" = c(rep(0, 10), rep(1, 0)),
+    "sex" = rep(c("F", "M"), 5),
+    "stage" = rep(c("I", "II"), 5)) %>%
+    mutate(sample_id = as.character(1:nrow(.)))
+
+  sub <- bm %>%
+    subset_by_frequency(t = .1, other_vars = stage, by = sex)
+
+  expect_equal(setdiff(names(bm), names(sub)), c("gene0"))
+
+  bm1 <- bind_rows(
+    "gen50" = c(rep(0, 5), rep(1, 5)),
+    "gene20" = c(rep(0, 8), rep(1, 2)),
+    "gene0" = c(rep(0, 10), rep(1, 0)),
+    "sex" = c(rep("F", 4), rep("M", 4), "F", "M"),
+    "stage" = rep(c("I", "II"), 5)) %>%
+    mutate(sample_id = as.character(1:nrow(.)))
+
+  sub1 <- bm1 %>%
+    subset_by_frequency(t = .25, other_vars = stage, by = sex)
+
+  sub2 <- bm1 %>%
+    subset_by_frequency(t = .85, other_vars = stage, by = sex)
+
+  expect_equal(setdiff(names(bm1), names(sub1)), c("gene20", "gene0"))
+
+  expect_equal(setdiff(names(bm1), names(sub2)), c("gen50", "gene20", "gene0"))
+})
+
+
+test_that("Check categorical `by` variable works", {
+
+  bm <- bind_rows(
+    "gen50" = c(rep(0, 5), rep(1, 5)),
+    "gene20" = c(rep(0, 8), rep(1, 2)),
+    "gene10" = c(rep(0, 9), rep(1, 1)),
+    "gene0" = c(rep(0, 10), rep(1, 0)),
+    "sex" = c(rep("F", 4), rep("M", 4), "F", "M"),
+    "stage" = rep(c("I", "II"), 5),
+    "grade" = c(rep(1:4, 2), 1, 2)) %>%
+    mutate(sample_id = as.character(1:nrow(.)))
+
+  sub <- subset_by_frequency(bm, other_vars = c("sex", "stage"), by = grade)
+
+  sub1 <- subset_by_frequency(bm, t = 0.35, other_vars = c("sex", "stage"), by = grade)
+
+  expect_equal(setdiff(names(bm), names(sub)), c("gene0"))
+
+  expect_equal(setdiff(names(bm), names(sub1)), c("gene20", "gene10", "gene0"))
+})
+
